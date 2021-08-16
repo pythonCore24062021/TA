50540c7c35450c26a879bf6d73ad93bd8bef439e
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-16 22:40:40 +0300

    veb

---
+++ lessons/lesson02/New Text Document.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome('C:\Users\rrasi\PycharmProjects\TA\Driver\chromedriver.exe')
+browser.get("https://google.com/")
