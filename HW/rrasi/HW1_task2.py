ff62ea7b54f09bdd24238776bf920a10a715d551
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-26 20:06:02 +0300

    hw

--- HW/rrasi/HW1_task2.py
+++ HW/rrasi/HW1_task2.py
@@ -36,11 +36,15 @@ class FirstSeleniumTest(unittest.TestCase):
         search_input.send_keys("mac")
         search_input.send_keys(Keys.ENTER)
 #        element.btm.click()
-        time.sleep(3)
+#        time.sleep(3)
+        element2 = self.driver.find_element_by_xpath('//*[@id="content"]/div/section/form/ul/p')
+
+        self.assertEqual("No results found.", element2.text)
+        self.assertEqual(element2.value_of_css_property("color"), 'rgba(68, 68, 68, 1)')
 
 # Verify that at least one search result contains the search phrase
-        xpath = f"//*[@id="search"]/input']//*[contains(text(), '{PHRASE}')]"
-        phrase_results = browser.find_elements_by_xpath(xpath)
-        assert len(phrase_results) > 0
+#        xpath = f"//*[@id="search"]/input"]/input']//*[contains(text(), '{PHRASE}')]"
+#        phrase_results = self.find_elements_by_xpath(xpath)
+#        assert len(phrase_results) > 0
 
 
