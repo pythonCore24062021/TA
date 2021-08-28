6cd49f2a6715123037b8b5921b62c95e07fff9fa
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-28 17:53:25 +0300

    hw

--- HW/rrasi/HW1_task1.py
+++ HW/rrasi/HW1_task1.py
@@ -36,23 +36,14 @@ class PythonOrgSearch(unittest.TestCase):
             "#form-currency > div > button > span"
         )
         currency_btm.click()
-        reg_btn = self.driver.find_element_by_css_selector(
-            "#top-links > ul > li.dropdown.open > ul > li:nth-child(1) > a")
-        reg_btn.click()
-        first_name_input = self.driver.find_element_by_css_selector("#input-firstname")
-        first_name_input.send_keys("lhalam")
-        email_input = self.driver.find_element_by_css_selector("#input-email")
-        email_input.send_keys("lhalam@l")
+        eur_btn = self.driver.find_element_by_css_selector(
+            "#form-currency > div > ul > li:nth-child(1) > button")
+        eur_btn.click()
 
-        continue_btn = self.driver.find_element_by_css_selector("#content > form > div > div > input.btn.btn-primary")
-        continue_btn.click()
+        search_input = self.driver.find_element_by_css_selector("#search > input")
+        search_input.send_keys("mac")
+        search_btn = self.driver.find_element_by_css_selector("#search > span > button > i")
+        search_btn.click()
+        products = self.driver.find_elements_by_class_name("product-thumb")
 
-        last_name_error_lb = self.driver.find_element_by_css_selector("#account > div:nth-child(4) > div > div")
-        self.assertEqual(last_name_error_lb.text,
-        "Last Name must be between 1 and 32 characters!")
-        try:
 
-            first_name_error_lb = self.driver.find_element_by_css_selector("#account > div:nth-child(3) > div > div")
-        except:
-            first_name_error_lb = None
-        self.assertIsNone(first_name_error_lb)
