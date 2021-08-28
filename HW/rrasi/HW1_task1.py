cff2ad243d95e515bd2fa9f0c72193b466d8d0a8
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-29 00:41:44 +0300

    hw

--- HW/rrasi/HW1_task1.py
+++ HW/rrasi/HW1_task1.py
@@ -5,10 +5,10 @@
 #Check out three cases of choosing on webpage: Dollar, Euro and Pound Sterling;
 
 import time
+import re
 import unittest
 
 from selenium import webdriver
-
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -16,7 +16,6 @@ class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         self.driver.get("http://taqc-opencart.epizy.com/")
-
         time.sleep(2)
 
     def tearDown(self):
@@ -27,23 +26,23 @@ class PythonOrgSearch(unittest.TestCase):
         cls.driver = webdriver.Chrome()
         cls.driver.maximize_window()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
 
-    def checkCurrencyChange(self):
-        currency_btm = self.driver.find_element_by_css_selector(
-            "#form-currency > div > button > span"
-        )
-        currency_btm.click()
-        eur_btn = self.driver.find_element_by_css_selector(
-            "#form-currency > div > ul > li:nth-child(1) > button")
+    def test_checkCurrencyChange(self):
+        All_currencies = ['$', '€', '£']
+        currency_btn = self.driver.find_element_by_css_selector("#form-currency")
+        currency_btn.click()
+        eur_btn = self.driver.find_element_by_css_selector("#form-currency > div > ul > li:nth-child(1) > button")
         eur_btn.click()
-
-        search_input = self.driver.find_element_by_css_selector("#search > input")
-        search_input.send_keys("mac")
-        search_btn = self.driver.find_element_by_css_selector("#search > span > button > i")
-        search_btn.click()
         products = self.driver.find_elements_by_class_name("product-thumb")
-
-
+        currencies = []
+        for product in products:
+            currencies.append(product.find_element_by_xpath('.//div/button/strong').text)
+        self.assertListEqual(currencies, ['€'])
+        currency_btn = self.driver.find_element_by_css_selector("#form-currency")
+        currency_btn.click()
+        usd_btn = self.driver.find_element_by_css_selector("#form-currency > div > ul > li:nth-child(3) > button")
+        usd_btn.click()
+        currency_btn = self.driver.find_element_by_css_selector("#form-currency")
+        currency_btn.click()
+        pound_btn = self.driver.find_element_by_css_selector("#form-currency > div > ul > li:nth-child(2) > button")
+        pound_btn.click()
--- HW/rrasi/HW1_task3.py
+++ HW/rrasi/HW1_task3.py
@@ -2,3 +2,16 @@
 #Please develop next methods in the FirstSeleniumTest class
 
 #checkAddToCart method - to check the functionality of adding items 'Xiaomi Mi 8' and 'MacBook' to cart.
+
+
+def checkCurrencyChange(self):
+    currency_btm = self.driver.find_element_by_css_selector(
+        "#form-currency > div > button > span"
+    )
+    currency_btm.click()
+    eur_btn = self.driver.find_element_by_css_selector(
+        "#form-currency > div > ul > li:nth-child(1) > button")
+    eur_btn.click()
+
+    search_input = self.driver.find_elements_by_xpath('.//div[2]/div[1]/div/div[2]/p[2]').text
+    search_input.send_keys("€")
