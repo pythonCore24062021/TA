825193a052f8ee66800f0e3303e2a6a104135e56
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-29 23:53:15 +0300

    hw

--- HW/rrasi/HW1_task3.py
+++ HW/rrasi/HW1_task3.py
@@ -35,10 +35,40 @@ class PythonOrgSearch(unittest.TestCase):
         cls.driver.quit()
 
     def test_checkAddToCart (self):
-        my_acount_btm = self.driver.find_element_by_css_selector("#top-links > ul > li.dropdown > a > span.hidden-xs.hidden-sm.hidden-md")
-        my_acount_btm.click()
-        reg_btn = self.driver.find_element_by_css_selector("#top-links > ul > li.dropdown.open > ul > li:nth-child(1) > a")
-        reg_btn.click()
+        search_input = self.driver.find_element_by_css_selector("#search > input")
+        search_input.send_keys("Xiaomi Mi 8")
+        search_button = self.driver.find_element_by_css_selector('#search > span > button > i')
+        search_button.click()
+        add_to_cart = self.driver.find_element_by_css_selector('#content > div:nth-child(8) > div > div > div:nth-child(2) > div.button-group > button:nth-child(1) > span')
+        add_to_cart.click()
+        option_btn = self.driver.find_element_by_css_selector('#input-option224')
+        option_btn.click()
+        select_size = find_element_by_css_selector('#input-option224 > option:nth-child(2)')
+        select_size.click()
+        add_product_btn = find_element_by_css_selector('# button-cart')
+        add_product_btn.click()
+
+        search_input = self.driver.find_element_by_css_selector("#search > input")
+        search_input.send_keys("MacBook")
+        search_button = self.driver.find_element_by_css_selector('#search > span > button > i')
+        search_button.click()
+        add_second_item_button = self.driver.find_element_by_css_selector(' # content > div:nth-child(8) > div:nth-child(1) > div > div:nth-child(2) > div.button-group > button:nth-child(1) > span')
+        add_second_item_button.click()
+        open_cart = WebDriverWait(self.driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@id="cart"]'))
+        )
+        open_cart.click()
+        check_add_to_cart = self.driver.find_elements_by_css_selector("td.text-left > a")#top-links > ul > li:nth-child(4) > a
+        check_cart_items = list()
+        for cart_item in check_add_to_cart:
+            check_cart_items.append(cart_item.text)
+
+        expected_results = ["Xiaomi Mi 8", "MacBook"]
+
+        self.assertTrue(set(check_cart_items) & set(expected_results))
+
+
+
 
 
 
