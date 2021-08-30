29a01d7df6b0154530e75961a604b5ff0863370f
Ruslana Rasiak <rusichka85d@gmail.com>
2021-08-30 10:26:35 +0300

    hw3'

---
+++ HW/rrasi/HW3_task1.py
@@ -0,0 +1,7 @@
+# HW04
+#Please, write integration tests by Selenium WebDriver to verify some features of Open Cart Application.
+# Search for web elements only by XPAth. Please develop next methods in the SearchXPathTest class
+
+# - loginByCssSelectoroginByXPath method - to check the functionality of login;
+
+
