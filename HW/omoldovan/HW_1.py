2f9fd2b2a9e43269af4b0a59aa33309d08146a85
Oleksandr Moldovan <alexmoldovan1027@gmail.com>
2021-09-02 05:53:38 +0300

    dasd

---
+++ HW/omoldovan/HW_1.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+
+class FirstSeleniumTest(unittest.TestCase):
+    driver=None
+
+    def setUp(self):
+        self.driver.get("http://taqc-opencart.epizy.com/")
+        time.sleep(2)
+
+    def tearDown(self):
+        time.sleep(2)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def test_euro_search(self):
+        currency = self.driver.find_element_by_css_selector('#form-currency > div > button > i')
+        currency.click()
+        euro.find_element_by_css_selector('#form-currency > div > ul > li:nth-child(1) > button')
+        euro.click()
+        currency_change = self.driver.find_element_by_css_selector('#cart-total')
+        self.assertEqual(currency_change.text,"0 item(s) - 0.00€")
