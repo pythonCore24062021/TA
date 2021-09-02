012a840e4472ec0f58eb92b9368d350937b831a1
Ruslana Rasiak <rusichka85d@gmail.com>
2021-09-02 12:45:40 +0300

    hw

--- HW/rrasi/HW3_task3.py
+++ HW/rrasi/HW3_task3.py
@@ -32,14 +32,17 @@ class PythonOrgSearch(unittest.TestCase):
         cls.driver.quit()
 
     def test_checkAddToCart (self):
-        search_input = self.driver.find_element_by_css_selector("#search > input")
+        search_input = self.driver.find_element_by_xpath(".//input[@name='search']")
         search_input.send_keys("Xiaomi Mi 8")
-        search_button = self.driver.find_element_by_css_selector('#search > span > button > i')
+        search_button = self.driver.find_element_by_xpath('//span[@class="input-group-btn"]')
         search_button.click()
-        add_to_cart = self.driver.find_element_by_css_selector('#content > div:nth-child(8) > div > div > div:nth-child(2) > div.button-group > button:nth-child(1) > span')
+        add_to_cart = self.driver.find_element_by_xpath('(//span[contains (text(), "Add to Cart")])')
         add_to_cart.click()
-        time.sleep(5)
-        option_btn = self.driver.find_element_by_css_selector('#input-option224')
+        time.sleep(3)
+        option_btn = self.driver.find_element_by_xpath('.//*[@id="input-option224"]')
+        time.sleep(2)
+#div[@class="form-group required"]
+#//select[@id="input-option224"]/option[@value="14"]
         option_btn.click()
         time.sleep(3)
         select_size = self.driver.find_element_by_xpath('.//*[@id="input-option224"]/option[2]')
@@ -48,21 +51,13 @@ class PythonOrgSearch(unittest.TestCase):
         add_product_btn = self.driver.find_element_by_xpath('//*[@id="button-cart"]')
         add_product_btn.click()
 
-        search_input = self.driver.find_element_by_css_selector("#search > input")
-        search_input.send_keys("MacBook")
-        search_button = self.driver.find_element_by_css_selector('#search > span > button > i')
-        search_button.click()
-        add_second_item_button = self.driver.find_element_by_css_selector(
-            ' #content > div:nth-child(8) > div:nth-child(1) > div > div:nth-child(2) > div.button-group > button:nth-child(1)')
-        add_second_item_button.click()
-        open_cart = self.driver.find_element_by_css_selector('#top-links > ul > li:nth-child(4) > a > span')
+
+        open_cart = self.driver.find_element_by_xpath('//span[contains (text(), "Shopping Cart")]')
+#
         open_cart.click()
-        check_add_to_cart = self.driver.find_elements_by_css_selector(
-            "#content > form > div > table > tbody > tr > td:nth-child(2) > a")  # td.text-left > a
-        check_cart_items = list()
-        for cart_item in check_add_to_cart:
-            check_cart_items.append(cart_item.text)
+        time.sleep(3)
+        check_add_to_cart = self.driver.find_element_by_xpath('//*[@id="content"]/form/div/table/tbody/tr/td[2]/a')
+#        ('//*[@id="content"]/form/div/table/tbody/tr/td[2]/a')
+        expected_results = ["Xiaomi Mi 8"]
 
-        expected_results = ["Xiaomi Mi 8", "MacBook"]
-
-        self.assertTrue(set(check_cart_items) & set(expected_results))
+        self.assertEqual(check_add_to_cart.text, 'Xiaomi Mi 8')
