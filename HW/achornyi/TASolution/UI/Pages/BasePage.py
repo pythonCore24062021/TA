f4e8d37e45fbcdb1b3a7aef37cc532434d894768
“Chornyi <chorny.andriy@gmail.com>
2021-09-07 21:13:22 +0300

    implemented get_url on BasePage level

--- HW/achornyi/TASolution/UI/Pages/BasePage.py
+++ HW/achornyi/TASolution/UI/Pages/BasePage.py
@@ -1,2 +1,7 @@
 class BasePage:
-    pass
+    def __init__(self, base_url: str):
+        self.relative_path = None
+        self.base_url = base_url
+
+    def get_url(self):
+        return self.base_url + self.relative_path
