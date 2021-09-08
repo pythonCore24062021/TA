ec4bad459ea89963f8abd647a91480b1e341c2e0
“Chornyi <chorny.andriy@gmail.com>
2021-09-08 12:36:07 +0300

    updated hw_02

--- HW/achornyi/ta_solution/tests/repositories/hw_02_repository.py
+++
@@ -1,2 +0,0 @@
-BASE_URL = "http://taqc-opencart.epizy.com/"  # TODO - Implement JSON Repository
-
--- HW/achornyi/ta_solution/tests/test_cases/hw_02.py
+++ HW/achornyi/ta_solution/tests/test_cases/hw_02.py
@@ -1,11 +1,10 @@
 from HW.achornyi.ta_solution.framework.web import drivers
-from HW.achornyi.ta_solution.tests.repositories import hw_02_repository
 from HW.achornyi.ta_solution.tests.test_cases.base_test_case import BaseTestCase
 
 
 class HW02Tests(BaseTestCase):
     def setUp(self):
-        drivers.current.get(hw_02_repository.BASE_URL)
+        super.setUp()
 
     def tearDown(self):
         pass
