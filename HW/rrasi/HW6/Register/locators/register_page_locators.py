3335c451cc16f58b350d7463380925da385e82da
Ruslana Rasiak <rusichka85d@gmail.com>
2021-09-08 17:20:43 +0300

    hw

--- HW/rrasi/HW6/Register/locators/register_page_locators.py
+++ HW/rrasi/HW6/Register/locators/register_page_locators.py
@@ -6,3 +6,8 @@ class RegisterPageLocators:
     LastName = (By.XPATH, '//*[@id="input-lastname"]')
     EMail = (By.XPATH, '//*[@id="input-email"]')
     Telephone = (By.XPATH, '//*[@id="input-telephone"]')
+    Address1 = (By.XPATH, '//*[@id="input-address-1"]')
+    city = (By.XPATH, '//*[@id="input-city"]')
+    PostCode = (By.XPATH, '//*[@id="input-postcode"]')
+    Country = (By.XPATH, '//*[@id="input-country"]')
+
