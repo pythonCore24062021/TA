cf7e207dd03f1868753a614f7ec76c3739d557b7
“Chornyi <chorny.andriy@gmail.com>
2021-09-08 12:35:50 +0300

    updated base_test_case

--- HW/achornyi/ta_solution/tests/test_cases/base_test_case.py
+++ HW/achornyi/ta_solution/tests/test_cases/base_test_case.py
@@ -3,6 +3,9 @@ from HW.achornyi.ta_solution.framework.web import drivers
 
 from selenium import webdriver
 
+from HW.achornyi.ta_solution.tests.repositories import base_repository
+from ui.pages.home_page import HomePage
+
 
 class BaseTestCase(unittest.TestCase):
     @classmethod
@@ -13,3 +16,7 @@ class BaseTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         drivers.current.quit()
+
+    def setUp(self):
+        drivers.current.get(base_repository.BASE_URL)
+        home_page = HomePage()
