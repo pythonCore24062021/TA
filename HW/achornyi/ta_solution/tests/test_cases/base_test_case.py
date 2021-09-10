e7b823845444aa28ae33307e809316884e85ecd3
“Chornyi <chorny.andriy@gmail.com>
2021-09-10 19:19:29 +0300

    updated base test case

--- HW/achornyi/ta_solution/tests/test_cases/base_test_case.py
+++ HW/achornyi/ta_solution/tests/test_cases/base_test_case.py
@@ -2,6 +2,7 @@ import unittest
 from HW.achornyi.ta_solution.tests.repositories import base_repository
 from framework.web.driver import Driver
 from framework.web.drivers import Drivers
+from ui.pages.home_page import _locators
 from ui.pages.home_page.home_page import HomePage
 
 
@@ -17,5 +18,5 @@ class BaseTestCase(unittest.TestCase):
         Drivers().get_current().quit()
 
     def setUp(self):
-        Drivers().get_current().get(base_repository.BASE_URL)
-        self.home_page = HomePage()
+        Drivers().get_current().get(base_repository.BASE_URL + _locators.RELATIVE_PATH)
+        self.start_page = HomePage()
