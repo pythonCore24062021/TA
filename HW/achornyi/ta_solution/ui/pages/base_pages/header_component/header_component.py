80d0a83cb3f0022d83783ecedc64ffa0e265275f
“Chornyi <chorny.andriy@gmail.com>
2021-09-10 19:20:27 +0300

    updated header component

--- HW/achornyi/ta_solution/ui/pages/base_pages/header_component/header_component.py
+++ HW/achornyi/ta_solution/ui/pages/base_pages/header_component/header_component.py
@@ -1,8 +1,18 @@
+import time
+
+from framework.web import element_factory
+from framework.web.elements.element import Element
+from framework.web.search import ID
+from framework.web.search import xpath
 from ui.pages.base_component import BaseComponent
 import ui.pages.base_pages.header_component._locators as locators
 
 
 class Header(BaseComponent):
-    def __call__(self, *args, **kwargs):
-        super().__init__(locators.WRAPPER)  # TODO - implement Element Factory on Framework to pass element
+    def __init__(self, wrapper: Element):
+        super().__init__(wrapper)
+        self.currency_button = self.wrapper.find_child_element(xpath(locators.CURRENCY_BTN_XPATH))
+
+    def set_currency(self, currency):
+        self.currency_button.click()
 
