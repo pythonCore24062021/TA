92279a1f7e2910e3414d10ca77862b280ca92929
“Chornyi <chorny.andriy@gmail.com>
2021-09-10 19:19:05 +0300

    updated base componentt

--- HW/achornyi/ta_solution/ui/pages/base_component.py
+++ HW/achornyi/ta_solution/ui/pages/base_component.py
@@ -4,10 +4,4 @@ from framework.web.search import Search
 
 class BaseComponent:
     def __init__(self, wrapper: Element):
-        self._wrapper = wrapper
-
-    def find_child_element(self, search: Search):
-        self._wrapper.find_child_element(search)
-
-    def find_child_elements(self, search: Search):
-        self._wrapper.find_child_elements(search)
+        self.wrapper = wrapper
