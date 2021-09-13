7b87f835221eb18989d89879f5700abb6432c3bd
“Chornyi <chorny.andriy@gmail.com>
2021-09-13 13:37:09 +0300

    implemented logger

---
+++ HW/achornyi/ta_solution/framework/system/system_logger.py
@@ -0,0 +1,38 @@
+import datetime
+import logging
+
+from framework.system._system_settings import SYSTEM_LOG_LEVEL
+
+_log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+
+# level_name_to_level = {
+#     "CRITICAL": logging.CRITICAL,
+#     "FATAL": logging.FATAL,
+#     "ERROR": logging.ERROR,
+#     "WARNING": logging.WARNING,
+#     "WARN": logging.WARN,
+#     "INFO": logging.INFO,
+#     "DEBUG": logging.DEBUG,
+#     "NOTSET": logging.NOTSET
+# }
+
+def get_file_handler():
+    file_handler = logging.FileHandler(f"{datetime.date.today()}.log")
+    file_handler.setLevel(SYSTEM_LOG_LEVEL)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(SYSTEM_LOG_LEVEL)
+    stream_handler.setFormatter(logging.Formatter(_log_format))
+    return stream_handler
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(SYSTEM_LOG_LEVEL)
+    logger.addHandler(get_file_handler())
+    logger.addHandler(get_stream_handler())
+    return logger
